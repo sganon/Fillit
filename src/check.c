46b0bf966df6342853880d231caefc52289277c1
sganon <sganon@student.42.fr>
2015-12-04 19:59:14 +0100

    Depot de fonctions de check et fonction de conversions de '#' en lettre

---
+++ src/check.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sganon <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/04 18:28:21 by sganon            #+#    #+#             */
+/*   Updated: 2015/12/04 19:50:46 by sganon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fillit.h"
+
+int		check_init(char *str)
+{
+	int	i;
+
+	i = 20;
+	while (str[i])
+	{
+		if (str[i] != '\n')
+			return (0);
+		i = i + i + 1;
+	}
+	return (1);
+}
+
+int		check_for_char(char **tab)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while(tab[i])
+	{
+		j = 0;
+		while (tab[i][j])
+		{
+			if (tab[i][j] != '.' && tab[i][j] != '#')
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int		check_for_size(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		if (ft_strlen(tab[i]) != 4)
+			return (0);
+		i++;
+	}
+	if (i % 4)
+		return (0);
+	return (1);
+}
